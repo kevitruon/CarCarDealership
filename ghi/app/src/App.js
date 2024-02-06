ccab5d3e87a4ae1c03b3e9541a46393605731aa4
kevitruon <146457163+kevitruon@users.noreply.github.com>
2024-02-06 13:58:31 -0800

    Added Routes to NavBar

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import Nav from './Nav';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage";
+import Nav from "./Nav";
 
 function App() {
   return (
@@ -8,7 +8,52 @@ function App() {
       <Nav />
       <div className="container">
         <Routes>
-          <Route path="/" element={<MainPage />} />
+          <Route path="/manufactures" element= {<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/create-manufacture" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/models" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/create-models" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/automobiles" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/create-auto" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/salespeople" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/add-salesperson" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/customer" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/sale" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/sales-history" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/technicians" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/add-technician" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/service-appointment" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/create-service-appointment" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/service-history" element={<MainPage />} />
         </Routes>
       </div>
     </BrowserRouter>
