9e2d62103178b8153b5b31b91288c5f724828732
kevitruon <146457163+kevitruon@users.noreply.github.com>
2024-02-05 17:14:29 -0800

    Fixed Poller

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -11,14 +11,14 @@ django.setup()
 
 # Import models from service_rest, here. Ignore vs-code error hinting
 # from service_rest.models import Something
-from api.service_rest.models import AutomobileVO
+from service_rest.models import AutomobileVO
 
 def get_autos():
-    response = requests.get("http://inventory-api:8000/api/automobiles/")
+    response = requests.get('http://project-beta-inventory-api-1:8000/api/automobiles')
     content = json.loads(response.content)
     for auto in content["autos"]:
         AutomobileVO.objects.update_or_create(
-            import_href = auto["href"],
+            import_href=auto["href"],
             defaults={
                 "vin":auto["vin"],
                 "sold":auto["sold"]
@@ -30,7 +30,7 @@ def poll():
         print('Service poller polling for data')
         try:
             get_autos()
-            print("Polling autos")
+            print("Found Auto Data")
         except Exception as e:
             print(e, file=sys.stderr)
 
